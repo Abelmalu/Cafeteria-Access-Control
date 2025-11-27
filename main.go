5b140a6a0a3350d69dfce31f780979f01d63ee35
abel malu <abellmalu@gmail.com>
2025-11-27 14:04:48 +0300

    embedding files done

--- main.go
+++ main.go
@@ -2,20 +2,38 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"io"
+	"os"
+
+	// // "io"
+	_ "embed"
+	// "os"
 )
 
-func expensiveCall() {
-
-	fmt.Println("this will be delayed")
-}
+//go:embed sql/migrations/example.txt
+var example string
 
 func main() {
 
-	fmt.Println("hellow ")
+	// _, err := os.Create("c:\\users\\abell\\desktop\\mydesktop.txt")
+	// //file := os.Create("")
 
-	time.Sleep(10 * time.Second)
+	// if err != nil {
 
-	expensiveCall()
+	// 	fmt.Println(err)
+	// }
+
+	fmt.Println(example)
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("")
+
+	fmt.Println("this is using os.Open")
+	file, _ := os.Open("sql/migrations/example.txt")
+
+	fileData, _ := io.ReadAll(file)
+
+	fmt.Println(string(fileData))
 
 }
